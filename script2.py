8b20396fa048efd590d4ac3dad8f40becff3add8
Anush Devv <devadhasan.a@husky.neu.edu>
2017-07-09 22:20:55 +0530

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,17 +1,17 @@
 from xlrd import open_workbook
 from sklearn import svm
+from sklearn.svm import SVC
 import numpy as np
 from scipy import stats
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
-from sklearn.linear_model import Lasso, LassoLarsCV, RandomizedLasso, MultiTaskLasso
+from sklearn.linear_model import Lasso, LassoLarsCV, LassoCV
 from sklearn import tree, datasets, preprocessing
 from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
-from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import cross_val_score, KFold, GridSearchCV
 from sklearn.ensemble import AdaBoostClassifier
-
-import matplotlib.pyplot as plt
-import time
+from sklearn.metrics import accuracy_score
+from statistics import mean
 
 #import data files
 data1 = open_workbook('/Users/anush/Desktop/data1.xlsx').sheet_by_index(0)
@@ -28,38 +28,83 @@ for i in range(1,data0.nrows):
 	g0.append(tmp0)
 
 #combine arrays across classes
-data = np.concatenate((g1,g0))
-x = np.array(data)
+x_0 = np.concatenate((g1,g0))
+x_0 = np.array(x_0)
+
 #take z-scores 
-x = stats.zscore(x, axis=0)
+x_0 = stats.zscore(x_0, axis=0)
+
 #create labels for data set (1/0)
-y = np.array(np.concatenate((np.ones(len(g1)),np.zeros(len(g0)))))
+y_0 = np.array(np.concatenate((np.ones(len(g1)),np.zeros(len(g0)))))
 
-#Least Angle Regression for optimal alpha selection
-model = LassoLarsCV(cv=20).fit(x, y)
-clf = Lasso(alpha=model.alpha_)
-clf.fit(x,y)
+accuracies=[]
 
-#print clf.coef_
+kf1 = KFold(n_splits=10)
+for train_index1, test_index1 in kf1.split(x_0):
+	
+	x1, x_test1 = x_0[train_index1], x_0[test_index1]
+	y1, y_test1 = y_0[train_index1], y_0[test_index1]
+	
+	nfold_inner = 10
+	features=np.zeros((nfold_inner,80))
+	count1=0
+	kf2 = KFold(n_splits=nfold_inner)
+	for train_index2, test_index2 in kf2.split(x1):
+		x2, x_test2 = x1[train_index2], x1[test_index2]
+		y2, y_test2 = y1[train_index2], y1[test_index2]
 
-#Select features satisfying coefficient threshold
-sfm = SelectFromModel(clf, threshold=0.05)
-sfm.fit(x,y)
-#apply selected features to original data set 
-x = sfm.transform(x)
+		#Least Angle Regression for optimal alpha selection
+		model = LassoLarsCV(cv=20).fit(x2, y2)
 
-#generate svm model for classification 
-clf = svm.SVC(kernel='rbf')
+		clf = Lasso(alpha=model.alpha_)
+		clf.fit(x2,y2)
+		count2=0
+		for i in clf.coef_:
+			if abs(i)>0.005:
+				features[count1,count2]=1
+			count2+=1
+		count1+=1
+		
+	features_select=[]
+	for i in range(80):
+		feature_count=0
+		for j in range(nfold_inner):
+			if features[j,i]==1:
+				feature_count+=1
+		
+		if feature_count==nfold_inner:
+			features_select.append(i)
+	x1 = x1[:,features_select]
+	x_test1 = x_test1[:,features_select]
+	print x1.shape
+	'''
+	#Select features satisfying coefficient threshold
+	sfm = SelectFromModel(clf, threshold=1e-50)
+	sfm.fit(x2,y2)
+	
+	#apply selected features to original data set 
+	x2 = sfm.transform(x2)
+	x_test2 = sfm.transform(x_test2)
+	'''
+	#generate svm model for classification 
+	#gamma_list = dict(gamma= [i*0.0001 for i in range(5, 1250, 5)])
+	#clf1 = GridSearchCV(estimator=SVC(), param_grid=gamma_list)
+	#clf1.fit(x1, y1)
+	clf = svm.SVC(kernel='rbf')#, C=clf1.best_estimator_.C)
+	clf.fit(x1,y1)
+	y_pred = clf.predict(x_test1)
+	accuracy = accuracy_score(y_test1, y_pred)
+	accuracies.append(accuracy) 
+print accuracies
 
-#clf = KNeighborsClassifier(n_neighbors=5, algorithm='auto')
-#clf = RandomForestClassifier(n_estimators=100)
-#bdt = AdaBoostClassifier(clf, algorithm="SAMME", n_estimators=500)
+	#clf = KNeighborsClassifier(n_neighbors=5, algorithm='auto')
+	#clf = RandomForestClassifier(n_estimators=100)
+	#bdt = AdaBoostClassifier(clf, algorithm="SAMME", n_estimators=500)
 
-#stratified k-fold cross validation
-scores = cross_val_score(clf, x, y, cv=20)
+	#stratified k-fold cross validation
+	#scores = cross_val_score(clf, x, y, cv=20)
 
-print scores.mean(), "+/-", scores.std()
-
+	#print scores.mean(), "+/-", scores.std()
 '''kf = KFold(n_splits=20)
 for train_index, test_index in kf.split(x):
 	x_train, x_test = x[train_index], x[test_index]
